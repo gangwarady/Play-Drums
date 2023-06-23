992a5a224514294c9811ec1379669571969d6472
ADITYA SINGH GANGWAR <103820233+gangwarady@users.noreply.github.com>
2023-06-23 20:48:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    makeSound(this.innerHTML);
+    animateBtn(this.innerHTML);
+  });
+}
+
+document.addEventListener("keypress", function (event) {
+   makeSound(event.key); 
+   animateBtn(event.key);
+    
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+          var audio = new Audio("sounds/tom-1.mp3");
+          audio.play();
+          break;
+  
+        case "a":
+          var audio = new Audio("sounds/tom-2.mp3");
+          audio.play();
+          break;
+  
+        case "s":
+          var audio = new Audio("sounds/tom-3.mp3");
+          audio.play();
+          break;
+  
+        case "d":
+          var audio = new Audio("sounds/tom-4.mp3");
+          audio.play();
+          break;
+  
+        case "j":
+          var audio = new Audio("sounds/snare.mp3");
+          audio.play();
+          break;
+  
+        case "k":
+          var audio = new Audio("sounds/crash.mp3");
+          audio.play();
+          break;
+  
+        case "l":
+          var audio = new Audio("sounds/kick-bass.mp3");
+          audio.play();
+          break;
+  
+        default:
+          console.log(this.innerHTML);
+          break;
+      }
+}
+
+function animateBtn(key) {
+    var sel = "."+key;
+    var activeBtn = document.querySelector(sel);
+    activeBtn.classList.add("pressed");
+
+    setTimeout(function(){
+        activeBtn.classList.remove("pressed");
+    }, 100);
+
+    
+}
